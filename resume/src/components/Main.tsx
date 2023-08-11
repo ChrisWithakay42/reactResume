022b6c91f22d00edf7b8ae80ceb3dafac90d3b61
ChrisWithakay42 <codewithakay@gmail.com>
2023-08-11 14:54:02 +0100

    setting up main component and adding typing animation package

---
+++ resume/src/components/Main.tsx
@@ -0,0 +1,16 @@
+
+const Main = () => {
+    return (
+        <div id='main'>
+           <img alt='Backgroud-image' className='w-full h-screen object-cover object-left' src='https://images.unsplash.com/photo-1453928582365-b6ad33cbcf64?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2073&q=80'/>
+            <div className='w-full h-screen absolute top-0 bg-white/50'>
+                <div>
+                    <h1>Hello Friend! My name is Kris</h1>
+                    <h2></h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Main
