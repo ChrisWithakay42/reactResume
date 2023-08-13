83e1b8254673525351be92d15510d818e412441a
ChrisWithakay42 <codewithakay@gmail.com>
2023-08-13 12:19:12 +0100

    directory refactor. gl runner registration. basic ci file for test

--- cicd/bucket_api.py
+++ cicd/bucket_api.py
@@ -1,2 +1,23 @@
-import boto3
+import logging
+import os
 
+import boto3
+from botocore.exceptions import ClientError
+
+logger = logging.getLogger(__name__)
+
+
+def create_bucket(*, bucket_name):
+    try:
+        client = boto3.resource('s3')
+    except ClientError as e:
+        logger.error(e)
+        return False
+    else:
+        client.create_bucket(
+            Bucket=bucket_name,
+            BucketConfiguration={
+                'LocationConstraint': os.getenv('AWS_REGION')
+            }
+        )
+    return True
