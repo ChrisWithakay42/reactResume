433b8c68c2ce44e814ac4fbf8b1f848a2c5a8623
ChrisWithakay42 <codewithakay@gmail.com>
2023-08-14 13:05:57 +0100

    Re-phrase work items and profile

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -1,39 +1,45 @@
-
 const Profile = () => {
     return (
         <div id='profile' className='max-w-[90%] md:max-w-[1040px] m-auto md:pl-4 py-16'>
-            <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Profile</h1>
+            <h1 className='text-4xl font-bold text-center text-[#001b5e]'>
+                Profile
+            </h1>
             <p className='my-2 text-base font-normal text-stone-800'>
                 Through my self-driven journey, I've cultivated an in-depth and comprehensive understanding of the
                 entire Software Development Life Cycle (SDLC), from those crucial initial client interactions and
                 analytical phases to the meticulous task design and ultimate project deployment.
-                <br/>
-                <br/>
+
+                <br/><br/>
+
                 At the heart of my work lies a commitment to excellence and innovation. I strongly advocate for the
                 principles of Test-Driven Development (TDD), firmly believing that well-tested code not only ensures
                 reliability but also paves the way for elegant solutions that stand the test of time. This dedication
                 to meticulous testing allows me to craft software that not only functions flawlessly but also adapts
                 seamlessly to evolving requirements.
-                <br/>
-                <br/>
+
+                <br/><br/>
+
                 A fervent enthusiast of Object-Oriented Programming (OOP) design patterns, I am continually exploring
                 and expanding my knowledge in this domain. I believe that a solid foundation in OOP is pivotal to
                 creating software that is not only efficient but also highly maintainable and extensible. By staying
                 abreast of the latest advancements in the OOP landscape, I strive to infuse each project with the best
                 practices and techniques that empower it for long-term success.
-                <br/>
-                <br/>
+
+                <br/><br/>
+
                 I approach every project with unwavering dedication to delivering high-quality software solutions.
                 Whether it's a complex application or an elegant website, my goal is to fuse functionality with
                 aesthetics, ensuring that the end product not only meets but exceeds expectations. I thrive on
                 challenges and am driven by the satisfaction of creating solutions that make a real impact.
-                <br/>
-                <br/>
+
+                <br/><br/>
+
                 If you're looking for a junior engineer who brings both technical prowess and a creative approach to
-                the table, I'm excited to connect. <a href='#contact'>Let's collaborate and bring your next project to life!</a>
+                the table, I'm excited to connect.
+                <a className='text-[#ff6600]' href='#contact'> Let's collaborate and bring your next project to life!</a>
             </p>
         </div>
-    )
-}
+    );
+};
 
-export default Profile
+export default Profile;
--- frontend/src/components/Work.tsx
+++ frontend/src/components/Work.tsx
@@ -6,29 +6,41 @@ const data: WorkData[] = [
     {
         year: 2022,
         title: 'Backend Python Developer @Cloud.ini',
-        duration: '1 year 8 months',
-        details: 'Contract position via an umbrella company for Dragonfly Intelligence; where my primary focus involves ' +
-            'crafting data ingress and process APIs to cater to frontend and client requirements. This project was built with a ' +
-            'specs first approach using OpenAPI 3.0 specification standards. Technologies applied: Flask, Connextion, SQLAlchemy ' +
-            'Pydantic. Hosted on Google Cloud Platform.'
+        duration: '1 year 6 months',
+        details: 'I undertook a contract position through an umbrella company for Dragonfly Intelligence at Cloud.ini, ' +
+            'where my role centered around developing data ingress and process APIs to meet frontend and client needs. ' +
+            'Employing a specifications-first approach using OpenAPI 3.0 standards, I focused on creating a robust ' +
+            'foundation. Technologies employed encompassed Flask, Connextion, SQLAlchemy, and Pydantic. The project is ' +
+            'hosted on the Google Cloud Platform.'
 
     },
     {
         year: 2022,
         title: 'Fullstack Developer @CodeWithakay',
-        duration: '1 year 7 months',
-        details: 'As a large scale personal project I have been working on the development of a SaaS product. ' +
-            'TheCragStack is a comprehensive Client Management System with a focus on the indoor climbing centre niche ' +
-            'The Backend of the application is being built with Python-Django and DRF; while the Frontend leverages ReactJS ' +
-            'The DevOps is utilising GitLab pipelines hosted on E2C'
+        duration: 'present',
+        details: 'During this period, I embarked on a significant personal project, wholeheartedly dedicating myself to ' +
+            'the development of a SaaS product named TheCragStack. This comprehensive Client Management System was ' +
+            'meticulously tailored for the indoor climbing center niche. In crafting this system, I engineered the ' +
+            'backend using Python-Django and DRF, while the frontend was built upon ReactJS for a seamless user ' +
+            'experience. ' +
+            'To ensure streamlined deployment, we implemented a robust DevOps strategy, featuring GitLab pipelines ' +
+            'hosted on E2C. Furthermore, the application\'s deployment is optimized through AWS services, with the ' +
+            'utilization of Docker enhancing its portability and scalability. This comprehensive approach guarantees ' +
+            'efficient deployment and optimal performance, facilitating seamless user interactions.'
     },
     {
         year: 2020,
         title: 'Freelance Python Developer @Freelancer/UpWork/Fiverrr',
         duration: '2 years',
-        details: 'Primary focus during this period was building data acquisition pipelines powered by various python technologies such as: ' +
-            'requests + BeautifulSoup, Selenium, Scrapy for web interactions; psycopg & SQLAlchemy for database connections and interactions ' +
-            'plotly, seaborn, matplotlib for data visualization.'
+        details: 'Over the course of two years as a freelance developer, my primary focus revolved around constructing ' +
+            'data acquisition pipelines using a range of Python tools like requests, BeautifulSoup, Selenium, and Scrapy.' +
+            ' These technologies facilitated seamless web interactions, while psycopg and SQLAlchemy ensured robust ' +
+            'database connections for efficient data handling. I also employed plotly, seaborn, and matplotlib to craft ' +
+            'impactful data visualizations. ' +
+            'Additionally, I created custom on-demand social media bots for platforms such as Twitter, Instagram, and ' +
+            'Discord. By integrating official APIs from these platforms with relevant project-specific APIs, I ' +
+            'developed automated solutions that effectively engaged with social media, enhancing user interaction and ' +
+            'satisfaction.'
     },
 ]
 
