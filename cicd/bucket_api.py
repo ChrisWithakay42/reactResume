e6323b845300858d1d28908e90c9c5f463d7d931
ChrisWithakay42 <codewithakay@gmail.com>
2023-08-14 18:22:46 +0100

    deployment script

--- cicd/bucket_api.py
+++ cicd/bucket_api.py
@@ -1,7 +1,6 @@
 import json
 import logging
 import os
-from typing import Callable
 
 import boto3
 from botocore.exceptions import ClientError
--- cicd/deploy_website.py
+++ cicd/deploy_website.py
@@ -1,6 +1,13 @@
-def deploy_to_s3():
-    print('Hello World! It Works!!')
+import os
 
+from cicd.bucket_api import S3Manager
+
+
+def deploy_to_s3():
+    s3_manager = S3Manager()
+    s3_manager.get_or_create_bucket(bucket_name='codewithakay.com', region=os.getenv('AWS_REGION'))
+    s3_manager.configure_bucket_for_web_hosting(bucket_name='codewithakay.com', index_document='index.html')
+    s3_manager.deploy_to_bucket(bucket_name='codewithakay.com')
 
 if __name__ == '__main__':
     deploy_to_s3()
