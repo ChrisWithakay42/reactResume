d26bcd76de2ffabbac8383ea2f8c41d610ba0435
ChrisWithakay42 <codewithakay@gmail.com>
2023-08-16 18:57:49 +0100

    Installed react-helmet for metadata configuration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,11 +4,16 @@ import Work from "./components/Work.tsx";
 import Profile from "./components/Profile.tsx";
 import Projects from "./components/Projects.tsx";
 import {Contact} from "./components/Contact.tsx";
+import {Helmet} from "react-helmet"
+
 
 function App() {
 
     return (
         <div>
+            <Helmet>
+                <title>Code With a K</title>
+            </Helmet>
             <Sidenav />
             <Main />
             <Profile/>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {createHtmlPlugin} from "vite-plugin-html";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
       react(),
-      createHtmlPlugin({
-        inject: {
-          data: {
-            title: 'index',
-          }
-        }
-      })
   ],
 })
