d370805af5f3c3bbbb13de60158b886998a2d923
ChrisWithakay42 <codewithakay@gmail.com>
2023-08-17 13:34:39 +0100

    added dotenv module to top level req file

--- contat_us/main.py
+++ contat_us/main.py
@@ -1,2 +1,37 @@
+import json
+
+import boto3
+from botocore.exceptions import ClientError
+
+
 def send_mail(context, event):
-    ...
+    message_details = json.loads(event['body'])
+
+    name = message_details.get('name', None)
+    phone = message_details.get('phone', None)
+    email = message_details.get('email', None)
+    subject = message_details.get('subject', None)
+    message = message_details.get('message', None)
+
+    email_body = f'Name: {name}\nPhone: {phone}\nEmail: {email}\nMessage: {message}'
+
+    client = boto3.client('ses', region_name='eu-west-2')
+    try:
+        client.send_email(
+            Source='codewithakay@gmail.com',
+            Destination='kris@codewithakay.com',
+            Message={
+                'Subject': {'Data': subject},
+                'Body': {'Text': {'Data': email_body}}
+            }
+        )
+    except ClientError as e:
+        return {
+            'StatusCode': 503,
+            'body': json.dumps(f'Error: {str(e)}')
+        }
+    else:
+        return {
+            'StatusCode': 200,
+            'body': json.dumps('Email Sent Successfully')
+        }
