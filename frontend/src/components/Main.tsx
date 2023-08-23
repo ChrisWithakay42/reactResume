e3dbd6b658e0eba74d539c8b707aa161a80f04e5
ChrisWithakay42 <codewithakay@gmail.com>
2023-08-23 12:21:47 +0100

    fixed import issue

--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -1,5 +1,5 @@
 import {TypeAnimation} from 'react-type-animation'
-import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+// import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import main from "../assets/main.jpg"
 
 // src='https://images.unsplash.com/photo-1453928582365-b6ad33cbcf64?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2073&q=80'
