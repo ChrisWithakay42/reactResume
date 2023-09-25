c064523b46f30e6b2d2ef2e90bc95d73d7d317cd
ChrisWithakay42 <codewithakay@gmail.com>
2023-09-25 12:51:55 +0100

    cloud front call to invalidate cache after deploy to s3

---
+++ cicd/cloud_front.py
@@ -0,0 +1,13 @@
+import boto3
+
+from cicd.aws_wrapper import CloudFrontWrapper
+
+
+def invalidate_cache(distribution_id: str):
+    client = boto3.client('cloudfront', 'eu-west-2')
+    wrapper = CloudFrontWrapper(cloud_front_client=client)
+    wrapper.invalidate_cache(distribution_id)
+
+
+if __name__ == '__main__':
+    invalidate_cache('E33Q1FMLV0KCEL')
