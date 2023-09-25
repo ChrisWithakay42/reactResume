7c62457fbf9c7f150c0b09ffa869adb94a9308d9
ChrisWithakay42 <codewithakay@gmail.com>
2023-09-25 12:15:39 +0100

    license key update

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,6 @@ import './index.css'
 import { registerLicense } from '@syncfusion/ej2-base';
 
 const syncFusionLicense = import.meta.env.VITE_SYNCFUSION_LICENSE_KEY
-// registerLicense('Ngo9BigBOggjHTQxAR8/V1NGaF1cXGNCd0x0Rnxbf1xzZFZMY1VbRnRPIiBoS35RdUVrW3tfdXBcQ2ZbUEVy');
 registerLicense(syncFusionLicense)
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
